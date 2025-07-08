138350869c22231b6f0ae771f29a3b7af9587d02
doldollee00 <doldollee00@gmail.com>
2025-07-08 11:25:48 +0900

    hello world 추가 테스트

--- src/main/java/v1/Calculator.java
+++ src/main/java/v1/Calculator.java
@@ -2,6 +2,8 @@ package Project_Calculator;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world!!!!!");
+        System.out.println("hello world!!!!!");
+        System.out.println("hello world!!!!!");
     }
 }
