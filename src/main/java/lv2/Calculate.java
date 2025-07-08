cd56f033c2ec922cf7250765b8c36cc1523838f6
doldollee00 <doldollee00@gmail.com>
2025-07-08 18:35:09 +0900

    사ì칙연산 결과 저장 컬렉션 타입 설정

--- src/main/java/lv2/Calculate.java
+++ src/main/java/lv2/Calculate.java
@@ -1,27 +1,34 @@
 package lv2;
 
+import java.util.ArrayList;
+
 public class Calculate {
 
-    int result;
+    //연산 결과를 저장하는 컬렉션 타입
+    ArrayList<Integer> resulthome = new ArrayList<>();
 
     //사칙연산 수행
     public int sum(int value1, int value2) {
-        result = value1 + value2;
+        int result = value1 + value2;
+        resulthome.add(result);
         return result;
     }
 
     public int sub(int value1, int value2) {
-        result = value1 - value2;
+        int result = value1 - value2;
+        resulthome.add(result);
         return result;
     }
 
     public int mult(int value1, int value2) {
-        result = value1 * value2;
+        int result = value1 * value2;
+        resulthome.add(result);
         return result;
     }
 
     public int div(int value1, int value2) {
-        result = value1 / value2;
+        int result = value1 / value2;
+        resulthome.add(result);
         return result;
     }
 }
